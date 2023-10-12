e189ba7d1b6519b72bd3c3441b24aabfaf04f3ab
gpguedezm <gpguedezm@gmail.com>
2023-10-12 00:33:17 -0300

    Agregando checkbox, estilos, y configuración para github page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ListaTarea';
+  title = 'Lista de Tareas';
+
+  tasks: { name: string, completed: boolean }[] = [
+    { name: 'Comprar comida', completed: false },
+    { name: 'Entrenar', completed: false },
+    { name: 'Preparar comida', completed: false },
+  ]
+
+  newTask: string = '';
+
+  addTask(){
+    this.tasks.push({ name: this.newTask, completed: false});
+    this.newTask='';
+  }
+
+  doneTask(index: number){
+    this.tasks.splice(index, 1);
+  }
+
+
 }
+
+
